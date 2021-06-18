f1d0623b8c3483222db254fff0c49bbbcb8706c6
Vlad <giordanimo@gmail.com>
2021-06-18 11:12:59 +0300

    finish all main tickets

--- bin/main.rb
+++ bin/main.rb
@@ -28,9 +28,34 @@ magazines.each do |magazine|
   b = magazine[0].split(";");
   imported_magazines << Magazine.new(b[0], b[1], b[2], b[3])
 end
+
+def get_by_isbn(isbn, imported_books, imported_magazines)
+  imported_books.find_all { |book| book.isbn == isbn }.push(*imported_magazines.find_all { |magazine| magazine.isbn == isbn })
+end
+
+def get_by_authors(authors, imported_books, imported_magazines)
+  imported_books.find_all { |book| book.authors == authors }.push(*imported_magazines.find_all { |magazine| magazine.authors == authors })
+end
+
+def sort_by_title(imported_books, imported_magazines)
+  imported_books.push(*imported_magazines).sort_by(&:title)
+end
+p 'ALL IMPORTED AUTHORS'
 p imported_authors
 p '*'*100
+p 'ALL IMPORTED BOOKS'
 p imported_books
 p '*'*100
+p 'ALL IMPORTED MAGAZINES'
 p imported_magazines
+p '*'*100
+p 'GET BOOKS AND MAGAZINES BY ISBN'
+p get_by_isbn('3214-5698-7412', imported_books, imported_magazines)
+p '*'*100
+p 'GET BOOKS AND MAGAZINES BY AUTHORS'
+p get_by_authors('null-walter@echocat.org', imported_books, imported_magazines)
+p '*'*100
+p 'SORT BOOKS AND MAGAZINES BY TITLE'
+p sort_by_title(imported_books, imported_magazines)
+p '*'*100
 
